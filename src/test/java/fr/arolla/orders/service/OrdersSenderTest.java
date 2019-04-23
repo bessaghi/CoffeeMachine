0ce3592f2f733f64b79a91bbf5bec040a22b0fef
bessaghi <essaghir.bouchra@gmail.com>
2019-04-23 12:11:06 +0200

    Adding test case for the Orange Juice.

--- src/test/java/fr/arolla/orders/service/OrdersSenderTest.java
+++ src/test/java/fr/arolla/orders/service/OrdersSenderTest.java
@@ -11,23 +11,26 @@ public class OrdersSenderTest {
 	@Test
 	public void testSendOrder() {
 		// case enough money inserted
-		Order orderTeaSugar = new Order(Drinks.TEA,"1", 0.4);
+		Order orderTeaSugar = new Order(Drinks.TEA, "1", 0.4);
 		assertEquals("T:1:0", OrdersSender.sendOrder(orderTeaSugar));
 		
-		Order orderChocolateNoSugar = new Order(Drinks.CHOCOLATE,"0", 0.5);
+		Order orderChocolateNoSugar = new Order(Drinks.CHOCOLATE, "0", 0.5);
 		assertEquals("H::", OrdersSender.sendOrder(orderChocolateNoSugar));
 		
-		Order orderCoffeeSugar = new Order(Drinks.COFFEE,"2", 0.6);
+		Order orderCoffeeSugar = new Order(Drinks.COFFEE, "2", 0.6);
 		assertEquals("C:2:0", OrdersSender.sendOrder(orderCoffeeSugar));
+		
+		Order orderJuice = new Order(Drinks.ORANGE_JUICE, "0", 0.6);
+		assertEquals("O::", OrdersSender.sendOrder(orderJuice));
 		
 		assertEquals("M:Preparation in progress..", OrdersSender.forwardMessage("Preparation in progress.."));
 		
 		// case not enough money inserted
-		Order orderCoffeeSugarMoneyMissing = new Order(Drinks.COFFEE,"2", 0.3);
+		Order orderCoffeeSugarMoneyMissing = new Order(Drinks.COFFEE, "2", 0.3);
 		assertEquals("M:Credit not sufficient, please add 0.3 to complete your order.", OrdersSender.sendOrder(orderCoffeeSugarMoneyMissing));
 		
 		// case more money inserted
-		Order orderCoffeeSugarMoreMoney = new Order(Drinks.COFFEE,"2", 0.7);
+		Order orderCoffeeSugarMoreMoney = new Order(Drinks.COFFEE, "2", 0.7);
 		assertEquals("C:2:0", OrdersSender.sendOrder(orderCoffeeSugarMoreMoney));
 	}
 	
