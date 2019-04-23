1990c0f4630441fd25a2d424266ba4850c2af433
bessaghi <essaghir.bouchra@gmail.com>
2019-04-23 12:42:12 +0200

    Creating the CoffeeMachineReport class.

---
+++ src/main/java/fr/arolla/orders/service/CoffeeMachineReport.java
@@ -0,0 +1,8 @@
+package fr.arolla.orders.service;
+
+public class CoffeeMachineReport {
+
+	public static String printReport() {
+		return "The number of drinks sold so far is: 9.\nThe money earned is: 3.5";
+	}
+}
